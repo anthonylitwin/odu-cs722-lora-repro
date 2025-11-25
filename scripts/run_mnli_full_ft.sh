dac05283ef05d2ff10462f7eb11d9b4a8c346ccd
Brett Warren <bwarr011@odu.edu>
2025-11-24 21:44:35 -0500

    try different hyperparameters for mnli full ft

--- scripts/run_mnli_full_ft.sh
+++ scripts/run_mnli_full_ft.sh
@@ -19,11 +19,11 @@ python3 code/train_roberta_glue.py \
   --model_name roberta-base \
   --task_name mnli \
   --method full \
-  --max_seq_length 128 \
-  --per_device_train_batch_size 16 \
+  --max_seq_length 512 \
+  --per_device_train_batch_size 32 \
   --per_device_eval_batch_size 32 \
-  --learning_rate 5e-5 \
-  --warmup_steps 0 \
+  --learning_rate 2e-5 \
+  --warmup_steps 2200 \
   --num_train_epochs 3 \
   --seed 42 \
   --output_dir /home/$USER/odu-cs722-lora-repro/checkpoints/mnli_full_ft
